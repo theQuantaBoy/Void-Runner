e677d0f4ed871ede74847bd0fdb677fb445dd89b
mohsen <mohsensalah128@gmail.com>
2024-12-30 18:40:48 +0330

    new title design added

--- ASCII_ART.h
+++ ASCII_ART.h
@@ -26,4 +26,29 @@ char game_title[23][96] = {
     {"                                         ggg::::::ggg                                          "},
     {"                                            gggggg                                             "}};
 
+char game_title_design_2[23][100] = {
+    {"RRRRRRRRRRRRRRRRR                                                                                  "},
+    {"R::::::::::::::::R                                                                                 "},
+    {"R::::::RRRRRR:::::R                                                                                "},
+    {"RR:::::R     R:::::R                                                                               "},
+    {"  R::::R     R:::::R    ooooooooooo       ggggggggg   ggggg uuuuuu    uuuuuu       eeeeeeeeeeee    "},
+    {"  R::::R     R:::::R  oo:::::::::::oo    g:::::::::ggg::::g u::::u    u::::u     ee::::::::::::ee  "},
+    {"  R::::RRRRRR:::::R  o:::::::::::::::o  g:::::::::::::::::g u::::u    u::::u    e::::::eeeee:::::ee"},
+    {"  R:::::::::::::RR   o:::::ooooo:::::o g::::::ggggg::::::gg u::::u    u::::u   e::::::e     e:::::e"},
+    {"  R::::RRRRRR:::::R  o::::o     o::::o g:::::g     g:::::g  u::::u    u::::u   e:::::::eeeee::::::e"},
+    {"  R::::R     R:::::R o::::o     o::::o g:::::g     g:::::g  u::::u    u::::u   e:::::::::::::::::e "},
+    {"  R::::R     R:::::R o::::o     o::::o g:::::g     g:::::g  u::::u    u::::u   e::::::eeeeeeeeeee  "},
+    {"  R::::R     R:::::R o::::o     o::::o g::::::g    g:::::g  u:::::uuuu:::::u   e:::::::e           "},
+    {"RR:::::R     R:::::R o:::::ooooo:::::o g:::::::ggggg:::::g  u:::::::::::::::uu e::::::::e          "},
+    {"R::::::R     R:::::R o:::::::::::::::o  g::::::::::::::::g   u:::::::::::::::u  e::::::::eeeeeeee  "},
+    {"R::::::R     R:::::R  oo:::::::::::oo    gg::::::::::::::g    uu::::::::uu:::u   ee:::::::::::::e  "},
+    {"RRRRRRRR     RRRRRRR    ooooooooooo        gggggggg::::::g      uuuuuuuu  uuuu     eeeeeeeeeeeeee  "},
+    {"                                                  g:::::g                                          "},
+    {"                                      gggggg       g:::::g                                         "},
+    {"                                      g:::::gg   gg:::::g                                          "},
+    {"                                       g::::::ggg:::::::g                                          "},
+    {"                                        gg:::::::::::::g                                           "},
+    {"                                          ggg::::::ggg                                             "},
+    {"                                             gggggg                                                "}};
+
 #endif
