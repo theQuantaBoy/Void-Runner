3d07ac0cf41e734a5c859ebfe62baaf409673609
mohsen <mohsensalah128@gmail.com>
2024-12-30 15:51:18 +0330

    adding ASCII Art

---
+++ ASCII_ART.h
@@ -0,0 +1,29 @@
+#ifndef MYUTILS_H
+#define MYUTILS_H
+
+char game_title[23][96] = {
+    {"RRRRRRRRRRRRRRRRR                                                                              "},
+    {"R::::::::::::::::R                                                                             "},
+    {"R::::::RRRRRR:::::R                                                                            "},
+    {"RR:::::R     R:::::R                                                                           "},
+    {"  R::::R     R:::::R   ooooooooooo      ggggggggg   ggggguuuuuu    uuuuuu      eeeeeeeeeeee    "},
+    {"  R::::R     R:::::R oo:::::::::::oo   g:::::::::ggg::::gu::::u    u::::u    ee::::::::::::ee  "},
+    {"  R::::RRRRRR:::::R o:::::::::::::::o g:::::::::::::::::gu::::u    u::::u   e::::::eeeee:::::ee"},
+    {"  R:::::::::::::RR  o:::::ooooo:::::og::::::ggggg::::::ggu::::u    u::::u  e::::::e     e:::::e"},
+    {"  R::::RRRRRR:::::R o::::o     o::::og:::::g     g:::::g u::::u    u::::u  e:::::::eeeee::::::e"},
+    {"  R::::R     R:::::Ro::::o     o::::og:::::g     g:::::g u::::u    u::::u  e:::::::::::::::::e "},
+    {"  R::::R     R:::::Ro::::o     o::::og:::::g     g:::::g u::::u    u::::u  e::::::eeeeeeeeeee  "},
+    {"  R::::R     R:::::Ro::::o     o::::og::::::g    g:::::g u:::::uuuu:::::u  e:::::::e           "},
+    {"RR:::::R     R:::::Ro:::::ooooo:::::og:::::::ggggg:::::g u:::::::::::::::uue::::::::e          "},
+    {"R::::::R     R:::::Ro:::::::::::::::o g::::::::::::::::g  u:::::::::::::::u e::::::::eeeeeeee  "},
+    {"R::::::R     R:::::R oo:::::::::::oo   gg::::::::::::::g   uu::::::::uu:::u  ee:::::::::::::e  "},
+    {"RRRRRRRR     RRRRRRR   ooooooooooo       gggggggg::::::g     uuuuuuuu  uuuu    eeeeeeeeeeeeee  "},
+    {"                                                 g:::::g                                       "},
+    {"                                     gggggg      g:::::g                                       "},
+    {"                                     g:::::gg   gg:::::g                                       "},
+    {"                                      g::::::ggg:::::::g                                       "},
+    {"                                       gg:::::::::::::g                                        "},
+    {"                                         ggg::::::ggg                                          "},
+    {"                                            gggggg                                             "}};
+
+#endif
